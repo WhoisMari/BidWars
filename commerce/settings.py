61d212f858d2c146a49187957af29a16fbeb74f7
WhoisMari <mfilippozzi0@gmail.com>
2021-05-05 18:15:28 +0200

    Heroku files

--- commerce/settings.py
+++ commerce/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['auctions-mari.herokuapp.com', '127.0.0.1', 'bidwars.whoismari.dev']
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
 # Application definition
